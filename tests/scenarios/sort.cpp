bb858e4f77708ea70e2346fcc475aca1c1e3d848
Artem E. Knyazev <artem.e.knyazev@yandex.ru>
2018-04-09 16:41:34 +0300

    Simplify tests building

--- tests/scenarios/sort.cpp
+++ tests/scenarios/sort.cpp
@@ -1,5 +1,9 @@
+#define TEST
+
+#include <gtest/gtest.h>
 #include "algs/sort/selection.hpp"
-#include "gtest/gtest.h"
+#include "algs/sort/insertion.hpp"
+#include "algs/sort/shell.hpp"
 
 namespace {
     using namespace algs::sort;
@@ -20,6 +24,26 @@ namespace {
         std::sort(out.begin(), out.end());
 
         selection::sort_stl(in.begin(), in.end());
+        ASSERT_TRUE(std::is_sorted(in.begin(), in.end()));
+        ASSERT_EQ(in, out);
+    }
+
+    TEST(Sort, InsertionSort) {
+        std::vector<int> in = { 5, 4, 3, 2, 1 };
+        std::vector<int> out = in;
+        std::sort(out.begin(), out.end());
+
+        insertion::sort(in.begin(), in.end());
+        ASSERT_TRUE(std::is_sorted(in.begin(), in.end()));
+        ASSERT_EQ(in, out);
+    }
+
+    TEST(Sort, ShellSort) {
+        std::vector<int> in = { 5, 4, 3, 2, 1 };
+        std::vector<int> out = in;
+        std::sort(out.begin(), out.end());
+
+        shell::sort(in.begin(), in.end());
         ASSERT_TRUE(std::is_sorted(in.begin(), in.end()));
         ASSERT_EQ(in, out);
     }
