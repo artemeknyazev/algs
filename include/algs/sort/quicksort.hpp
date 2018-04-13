9117f97b871f02a8252fb3eb629bdbbf8aa09a67
Artem E. Knyazev <artem.e.knyazev@yandex.ru>
2018-04-13 16:02:52 +0300

    Dual pivot partitioning

--- include/algs/sort/quicksort.hpp
+++ include/algs/sort/quicksort.hpp
@@ -1,3 +1,7 @@
+#include <utility>
+
+// TODO: add 3-way partitioning;
+
 namespace algs::sort::quicksort {
     /**
      * Check if a collection is partitioned around a `pivot` element
@@ -11,6 +15,9 @@ namespace algs::sort::quicksort {
         RandomAccessIterator pivot,
         RandomAccessIterator end
     ) {
+        assert(begin <= pivot);
+        assert(pivot <= end);
+
         if (begin == end)
             return true;
         auto it = begin;
@@ -18,7 +25,7 @@ namespace algs::sort::quicksort {
             if (*it <= *pivot)
                 ++it;
             else {
-                std::cout << "aaa " << *it << std::endl;
+                //std::cout << "aaa " << *it << std::endl;
                 return false;
             }
         it = std::prev(end);
@@ -26,7 +33,54 @@ namespace algs::sort::quicksort {
             if (*pivot <= *it)
                 --it;
             else {
-                std::cout << "bbb " << *it << std::endl;
+                //std::cout << "bbb " << *it << std::endl;
+                return false;
+            }
+        return true;
+    }
+
+    /**
+     * Check if a collection is partitioned around dual pivots
+     **/
+    template<
+        typename RandomAccessIterator
+    >
+    bool
+    is_partitioned_dual_pivot(
+        RandomAccessIterator begin,
+        RandomAccessIterator left,
+        RandomAccessIterator right,
+        RandomAccessIterator end
+    ) {
+        assert(begin <= left);
+        assert(left <= right);
+        assert(right <= end);
+
+        if (begin == end)
+            return true;
+
+        auto it = begin;
+        while (it != left)
+            if (*it < *left)
+                ++it;
+            else {
+                //std::cerr << "aaa " << *it << " " << *left << std::endl;
+                return false;
+            }
+        it = std::next(left);
+        while (it != right)
+            if (*left <= *it && *it <= *right)
+                ++it;
+            else {
+                //std::cerr << "bbb " << *it << " " << *left << " " << *right << std::endl;
+                return false;
+            }
+        it = std::next(right);
+        while (it != end)
+            if (*left < *it)
+                ++it;
+            else {
+                //std::cerr << "ccc " << *it << " " << *right << std::endl;
                 return false;
             }
         return true;
@@ -35,6 +89,7 @@ namespace algs::sort::quicksort {
     /**
      * Partition a collection around a pivot element.
      * NOTE: Uses first element as a pivot
+     * NOTE: New element order: (<=p) p (>p)
      **/
     template<
         typename RandomAccessIterator
@@ -57,6 +112,56 @@ namespace algs::sort::quicksort {
         }
         std::iter_swap(begin, more);
         return more;
+    }
+
+    /**
+     * Partition a collection around two pivots
+     * NOTE: Uses first and last elements as pivots
+     * NOTE: New element order: (<lp) lp (lp <= & <= rp) rp (rp<)
+     **/
+    template<
+        typename RandomAccessIterator
+    >
+    std::pair<RandomAccessIterator, RandomAccessIterator>
+    partition_dual_pivot(
+        RandomAccessIterator begin,
+        RandomAccessIterator end
+    ) {
+        if (end <= begin)
+            return std::make_pair(begin, end);
+        auto p = begin; // left pivot
+        auto q = std::prev(end); // right pivot
+        if (*q < *p) // left pivot should be less than the right one
+            std::iter_swap(p, q);
+        // (p,l) contains elems less than the first pivot
+        // [l,k) contains elems between pivots
+        // [k,g] contains unpartitioned part of the collection
+        // (g,q) contains elems greater than the second pivot
+        auto l = std::next(begin);
+        auto g = std::prev(end);
+        for (auto k = l; k <= g; ++k) {
+            if (*k < *p) { // skip elems less than the left pivot
+                std::iter_swap(k, l);
+                ++l;
+                //std::cout << "a:  "; for (auto it = begin; it != end; ++it) std::cout << *it << ' '; std::cout << std::endl;
+            } else if (*q <= *k) { // found element in the left part gte than the right pivot
+                while (k < g && *q < *g) // skip elems less than the right pivot
+                    --g;
+                std::iter_swap(k, g); // swap left greater elem with right less elem
+                //std::cout << "b1: "; for (auto it = begin; it != end; ++it) std::cout << *it << ' '; std::cout << std::endl;
+                --g;
+                if (*k < *p) { // again skip elems less than the left pivot
+                    std::iter_swap(k, l);
+                    ++l;
+                }
+                //std::cout << "b2: "; for (auto it = begin; it != end; ++it) std::cout << *it << ' '; std::cout << std::endl;
+            }
+        }
+        --l;
+        ++g;
+        std::iter_swap(p, l);
+        std::iter_swap(q, g);
+        return std::make_pair(l, g);
     }
 
     /**
--- tests/scenarios/sort/quicksort.cpp
+++ tests/scenarios/sort/quicksort.cpp
@@ -5,53 +5,59 @@ namespace {
     REGISTER_TESTS(Sort, Quicksort_Sort, algs::sort::quicksort::sort)
 
     TEST(Sort, Quicksort_IsPartitioned) {
+        using algs::sort::quicksort::is_partitioned;
         { // check empty
             std::vector<int> original = {};
             auto pivot = original.begin();
-            ASSERT_TRUE(algs::sort::quicksort::is_partitioned(original.begin(), pivot, original.end()));
+            ASSERT_TRUE(is_partitioned(original.begin(), pivot, original.end()));
         }
         { // check one element
             std::vector<int> original = {1};
             auto pivot = original.begin();
-            ASSERT_TRUE(algs::sort::quicksort::is_partitioned(original.begin(), pivot, original.end()));
+            ASSERT_TRUE(is_partitioned(original.begin(), pivot, original.end()));
         }
         { // check sorted
             std::vector<int> original = {1,2,3,4,5,6,7,8,9,10};
             auto pivot = original.begin();
             while (pivot != original.end()) {
-                ASSERT_TRUE(algs::sort::quicksort::is_partitioned(original.begin(), pivot, original.end()));
+                ASSERT_TRUE(is_partitioned(original.begin(), pivot, original.end()));
                 ++pivot;
             }
         }
         { // check partitioned
             std::vector<int> original = {0,5,4,2,6,9,8,10,12,7};
             auto pivot = std::next(original.begin(), 4);
-            ASSERT_TRUE(algs::sort::quicksort::is_partitioned(original.begin(), pivot, original.end()));
+            ASSERT_TRUE(is_partitioned(original.begin(), pivot, original.end()));
         }
         { // check 3-way partitioned
             std::vector<int> original = {0,5,4,2,6,6,6,10,12,7};
             auto pivot = std::next(original.begin(), 4);
-            ASSERT_TRUE(algs::sort::quicksort::is_partitioned(original.begin(), pivot, original.end()));
+            ASSERT_TRUE(is_partitioned(original.begin(), pivot, original.end()));
             pivot = std::next(original.begin(), 5);
-            ASSERT_TRUE(algs::sort::quicksort::is_partitioned(original.begin(), pivot, original.end()));
+            ASSERT_TRUE(is_partitioned(original.begin(), pivot, original.end()));
             pivot = std::next(original.begin(), 6);
-            ASSERT_TRUE(algs::sort::quicksort::is_partitioned(original.begin(), pivot, original.end()));
+            ASSERT_TRUE(is_partitioned(original.begin(), pivot, original.end()));
         }
     }
 
     TEST(Sort, Quicksort_Partition) {
+        using algs::sort::quicksort::partition;
+        using algs::sort::quicksort::is_partitioned;
         using Container = std::vector<int>;
+        using value_type = typename Container::value_type;
         for (auto size : TEST_CONTAINER_SIZES) {
             for (int i = 0; i < std::max(1., std::log(size)); ++i) {
                 Container original(size);
-                fill_container(original.begin(), original.end(), 0, 20);
-                auto pivot = algs::sort::quicksort::partition(original.begin(), original.end());
-                ASSERT_TRUE(algs::sort::quicksort::is_partitioned(original.begin(), pivot, original.end()));
+                fill_container(original.begin(), original.end(), value_type(0), value_type(size));
+                auto pivot = partition(original.begin(), original.end());
+                ASSERT_TRUE(is_partitioned(original.begin(), pivot, original.end()));
             }
         }
     }
 
     TEST(Sort, Quicksort_Select) {
+        using algs::sort::shuffle;
+        using algs::sort::quicksort::select;
         using Container = std::vector<int>;
         using value_type = typename Container::value_type;
         std::random_device rd;
@@ -62,9 +68,24 @@ namespace {
                 Container original(size);
                 size_t k = dis(gen);
                 std::generate(original.begin(), original.end(), [n = 0] () mutable { return value_type(n++); });
-                algs::sort::shuffle(original.begin(), original.end());
-                auto it = algs::sort::quicksort::select(original.begin(), original.end(), k);
+                shuffle(original.begin(), original.end());
+                auto it = select(original.begin(), original.end(), k);
                 ASSERT_TRUE(*it == value_type(k));
+            }
+        }
+    }
+
+    TEST(Sort, Quicksort_Partition_DualPivot) {
+        using algs::sort::quicksort::partition_dual_pivot;
+        using algs::sort::quicksort::is_partitioned_dual_pivot;
+        using Container = std::vector<int>;
+        using value_type = typename Container::value_type;
+        for (auto size : TEST_CONTAINER_SIZES) {
+            for (int i = 0; i < std::max(1., std::log(size)); ++i) {
+                Container v(size);
+                fill_container(v.begin(), v.end(), value_type(0), value_type(size));
+                auto part = partition_dual_pivot(v.begin(), v.end());
+                ASSERT_TRUE(is_partitioned_dual_pivot(v.begin(), part.first, part.second, v.end()));
             }
         }
     }
